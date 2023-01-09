cf40ffc87752429a277980836df79afb408075e7
karelumair <umair.karel@sarvaha.com>
2023-01-09 12:42:55 +0530

    Added Create API for Quotes

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 from bson import json_util, ObjectId
 from dotenv import load_dotenv, find_dotenv
+from datetime import datetime
 import json
 import os
 
@@ -38,6 +39,28 @@ def get_quote(_id):
         quote = mongo_client.db.quotes.find_one_or_404(filter_)
         response = {
             "quote": getJson(quote)
+        }
+    except Exception as e:
+        response = {
+            "Error": str(e)
+        }
+
+    return jsonify(response)
+
+
+@app.route("/quotes/", methods=["POST"])
+def add_quote():
+    quotes_data = request.json
+    quotes_data["createdOn"] = datetime.utcnow()
+    quotes_data["updatedOn"] = datetime.utcnow()
+
+    try:
+        add = mongo_client.db.quotes.insert_one(quotes_data)
+        ref_id = add.inserted_id
+
+        response = {
+            "message": "Successfully added to the database.",
+            "_id": getJson(ref_id)
         }
     except Exception as e:
         response = {
