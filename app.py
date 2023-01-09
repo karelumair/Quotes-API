5e6f55d437a3b7535a981ed22e352babf2c00fc0
karelumair <umair.karel@sarvaha.com>
2023-01-09 16:36:22 +0530

    Project re-structured

--- app.py
+++ app.py
@@ -1,73 +1,26 @@
-from flask import Flask, request, jsonify
-from flask_pymongo import PyMongo
-from bson import json_util, ObjectId
+from flask import Flask
+from flask_restful import Api
+from database.db import init_db
+from resources.quote import QuotesApi, QuoteApi
 from dotenv import load_dotenv, find_dotenv
-from datetime import datetime
-import json
 import os
 
 load_dotenv(find_dotenv())
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-
-mongo_client = PyMongo(app)
-
-
-def getJson(data):
-    return json.loads(json_util.dumps(data))
+app.config["MONGODB_SETTINGS"] = {
+    "host": os.environ.get("MONGO_URI")
+}
 
 
-@app.route("/quotes/", methods=["GET"])
-def get_quotes():
-    quotes_data = mongo_client.db.quotes.find()
-    quotes = [quote for quote in quotes_data]   
-    count = len(quotes)
-
-    response = {
-        "quotes": getJson(quotes),
-        "count": count
-    }
-
-    return jsonify(response)
+# Initialize DB
+init_db(app)
 
 
-@app.route("/quotes/<_id>/", methods=["GET"])
-def get_quote(_id):
-    try:
-        filter_ = {"_id": ObjectId(_id)}
-        quote = mongo_client.db.quotes.find_one_or_404(filter_)
-        response = {
-            "quote": getJson(quote)
-        }
-    except Exception as e:
-        response = {
-            "Error": str(e)
-        }
-
-    return jsonify(response)
-
-
-@app.route("/quotes/", methods=["POST"])
-def add_quote():
-    quotes_data = request.json
-    quotes_data["createdOn"] = datetime.utcnow()
-    quotes_data["updatedOn"] = datetime.utcnow()
-
-    try:
-        add = mongo_client.db.quotes.insert_one(quotes_data)
-        ref_id = add.inserted_id
-
-        response = {
-            "message": "Successfully added to the database.",
-            "_id": getJson(ref_id)
-        }
-    except Exception as e:
-        response = {
-            "Error": str(e)
-        }
-
-    return jsonify(response)
+# Routes
+api = Api(app)
+api.add_resource(QuotesApi, "/quotes/")
+api.add_resource(QuoteApi, "/quotes/<id>/")
 
 
 if __name__ == "__main__":
