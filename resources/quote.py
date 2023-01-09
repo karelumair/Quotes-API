5d89da15d878b8f5b29e49de287d2fd833b4e54b
karelumair <umair.karel@sarvaha.com>
2023-01-09 17:08:20 +0530

    Fixed some api

--- resources/quote.py
+++ resources/quote.py
@@ -1,6 +1,7 @@
 from flask import Response, request
 from database.models import Quote
 from flask_restful import Resource
+from datetime import datetime
 
 class QuotesApi(Resource):
     def get(self):
@@ -9,18 +10,18 @@ class QuotesApi(Resource):
 
     def post(self):
         body = request.get_json()
-        movie =  Quote(**body).save()
-        id = movie.id
-        return {'id': str(id)}, 200
+        quote =  Quote(**body).save().to_json()
+        return Response(quote, mimetype="application/json", status=200)
         
 class QuoteApi(Resource):
     def put(self, id):
         body = request.get_json()
-        Quote.objects.get(id=id).update(**body)
+        body['updatedOn'] = datetime.utcnow()
+        quote = Quote.objects.get(id=id).update(**body)
         return {'id': str(id)}, 200
     
     def delete(self, id):
-        movie = Quote.objects.get(id=id).delete()
+        quote = Quote.objects.get(id=id).delete()
         return '', 200
 
     def get(self, id):
