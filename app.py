5ce0fcae4a5bae2ee1f92da953ffab96de6771af
karelumair <umair.karel@sarvaha.com>
2023-02-09 11:23:40 +0530

    Fixed Logger Param

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
+import logging
+import os
 from flask import Flask
 from flask_restful import Api
+from dotenv import load_dotenv, find_dotenv
 from database.db import init_db
 from resources.quote import QuotesApi, QuoteApi
 from resources.author import AuthorsApi, AuthorApi
-from dotenv import load_dotenv, find_dotenv
-from utils.logging import init_logger
-import logging
-import os
+from utils.logger import init_logger
 
 load_dotenv(find_dotenv())
+
+MONGO_URI = os.environ.get("MONGO_URI")
 
 logger = logging.getLogger("app")
 init_logger(logger)
 
 app = Flask(__name__)
-app.config["MONGODB_SETTINGS"] = {
-    "host": os.environ.get("MONGO_URI")
-}
+app.config["MONGODB_SETTINGS"] = {"host": MONGO_URI}
 
 
 # Initialize DB
